b22100e28285344734ee47d1207fd29bd99c840d
“zojian” <hellozojian@gmail.com>
2018-07-24 21:25:27 +0800

    create EmployeeDao

--- src/main/java/com/oocl/restfulpractice1/dao/EmployeeDao.java
+++ src/main/java/com/oocl/restfulpractice1/dao/EmployeeDao.java
@@ -1,4 +1,14 @@
 package com.oocl.restfulpractice1.dao;
 
-public interface EmployeeDao {
+
+import com.oocl.restfulpractice1.domain.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao{
+    List<Employee> getAllEmployees();
+    Employee addEmployee(Employee employee);
+    Employee deleteEmployee(int id);
+    Employee modifyEmployee(int id,Employee employee);
+    Employee searchEmployeeById(int id);
 }
