aab3e8781e2f15fdaec23f632e7c7d5725420f8a
“zojian” <hellozojian@gmail.com>
2018-07-24 21:23:51 +0800

    create Emplyoee Model

---
+++ src/main/java/com/oocl/restfulpractice1/RestfulPractice1Application.java
@@ -0,0 +1,12 @@
+package com.oocl.restfulpractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulPractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulPractice1Application.class, args);
+    }
+}
---
+++ src/main/java/com/oocl/restfulpractice1/dao/EmployeeDao.java
@@ -0,0 +1,4 @@
+package com.oocl.restfulpractice1.dao;
+
+public interface EmployeeDao {
+}
